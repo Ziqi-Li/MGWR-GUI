6766235e3f4b6565508fc17363ddd3922f37e1a5
kchenlun <kchenlun@gmail.com>
2025-08-21 19:12:17 -0400

    Add files via upload

--- mgwrlib/mgwr/gwr.py
+++ mgwrlib/mgwr/gwr.py
@@ -1536,27 +1536,8 @@ class MGWR(GWR):
       """
       Initialize class
       """
-      self.y = y 
-      self.sigma2_v1 = sigma2_v1
-      self.points = None
-      self.P = None
-      self.exog_resid = None
-      self.exog_scale = None
-      self.fit_params = None
-      self.n_jobs = n_jobs
-      self.name_x = name_x
       self.family = family
 
-      if offset is None:
-        self.offset = np.ones((len(y), 1))
-      else:
-        self.offset = offset * 1.0
-
-      GWR.__init__(self, coords, y, X, self.bw_init, family=self.family,
-                   sigma2_v1=sigma2_v1, kernel=kernel, fixed=fixed,
-                   constant=constant, spherical=spherical,
-                   hat_matrix=hat_matrix)
- 
       if isinstance(self.family, Gaussian):
         self.selector = selector
         self.bws = selector.bw[0]  #final set of bandwidth
@@ -1569,6 +1550,25 @@ class MGWR(GWR):
         self.bws = None
         self.bws_history = None
         self.bw_init = None
+
+      GWR.__init__(self, coords, y, X, self.bw_init, family=self.family,
+                   sigma2_v1=sigma2_v1, kernel=kernel, fixed=fixed,
+                   constant=constant, spherical=spherical,
+                   hat_matrix=hat_matrix)
+      self.y = y 
+      self.sigma2_v1 = sigma2_v1
+      self.points = None
+      self.P = None
+      self.exog_resid = None
+      self.exog_scale = None
+      self.fit_params = None
+      self.n_jobs = n_jobs
+      self.name_x = name_x
+      
+      if offset is None:
+        self.offset = np.ones((len(y), 1))
+      else:
+        self.offset = offset * 1.0
 
     def _chunk_compute_R(self, chunk_id=0):
         """
@@ -1758,12 +1758,14 @@ class MGWR(GWR):
             CCT = np.sum(np.array(rslt_list[1]), axis=0)
     
             w = np.ones(self.n)
+
             if self.hat_matrix:
                 R = np.hstack(rslt_list[2])
             else:
                 R = None
+
             return MGWRResults(self, params, predy, CCT, ENP_j, w, R, self.name_x)
-            
+
         elif isinstance(self.family, (Poisson, Binomial)):
             from .sel_bw import Sel_BW
 
@@ -1864,9 +1866,8 @@ class MGWR(GWR):
 
             final_model.selector = sel
             final_model.bws = np.array(bws_guass_wxz)
-            final_model.name_x = self.name_x
 
-            return MGWRResults(final_model, betas, mu, mgwr_rslt.raw_CCT, mgwr_rslt.ENP_j, w, mgwr_rslt.R, final_model.name_x)
+            return MGWRResults(final_model, betas, mu, mgwr_rslt.raw_CCT, mgwr_rslt.ENP_j, w, mgwr_rslt.R, self.name_x)
     
         else:
             raise NotImplementedError('N/A')
