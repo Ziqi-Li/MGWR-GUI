f6cd4fa2b93d041da7961b757f19bfc2d88225e2
Ziqi Li <liziqi1992@gmail.com>
2025-09-18 13:38:38 -0400

    Add files via upload

--- src/loader.py
+++ src/loader.py
@@ -7,48 +7,55 @@
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-import sys,os
+import sys, os
+
 
 class Ui_runningDialog(object):
     def setupUi(self, runningDialog):
         runningDialog.setObjectName("runningDialog")
-        runningDialog.resize(501, 351)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        runningDialog.resize(600, 400)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed,
+                                           QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(runningDialog.sizePolicy().hasHeightForWidth())
+        sizePolicy.setHeightForWidth(
+            runningDialog.sizePolicy().hasHeightForWidth())
         runningDialog.setSizePolicy(sizePolicy)
+        runningDialog.setMinimumSize(QtCore.QSize(600, 400))
         runningDialog.setModal(True)
-        self.loadingGIFLabel = QtWidgets.QLabel(runningDialog)
-        self.loadingGIFLabel.setGeometry(QtCore.QRect(110, 60, 50, 50))
-        self.loadingGIFLabel.setLayoutDirection(QtCore.Qt.LeftToRight)
-        self.loadingGIFLabel.setText("")
-        self.loadingGIFLabel.setAlignment(QtCore.Qt.AlignCenter)
-        self.loadingGIFLabel.setObjectName("loadingGIFLabel")
-        self.gridLayoutWidget = QtWidgets.QWidget(runningDialog)
-        self.gridLayoutWidget.setGeometry(QtCore.QRect(10, 10, 481, 331))
-        self.gridLayoutWidget.setObjectName("gridLayoutWidget")
-        self.gridLayout = QtWidgets.QGridLayout(self.gridLayoutWidget)
+        self.gridLayout_2 = QtWidgets.QGridLayout(runningDialog)
+        self.gridLayout_2.setObjectName("gridLayout_2")
+        self.gridLayout = QtWidgets.QGridLayout()
         self.gridLayout.setContentsMargins(20, 20, 20, 20)
         self.gridLayout.setObjectName("gridLayout")
         self.horizontalLayout = QtWidgets.QHBoxLayout()
         self.horizontalLayout.setObjectName("horizontalLayout")
-        self.label = QtWidgets.QLabel(self.gridLayoutWidget)
+        self.label = QtWidgets.QLabel(runningDialog)
+        font = QtGui.QFont()
+        font.setPointSize(9)
+        self.label.setFont(font)
         self.label.setObjectName("label")
         self.horizontalLayout.addWidget(self.label)
-        self.label_2 = QtWidgets.QLabel(self.gridLayoutWidget)
-        self.label_2.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
+        self.label_2 = QtWidgets.QLabel(runningDialog)
+        font = QtGui.QFont()
+        font.setPointSize(9)
+        self.label_2.setFont(font)
+        self.label_2.setAlignment(QtCore.Qt.AlignRight
+                                  | QtCore.Qt.AlignTrailing
+                                  | QtCore.Qt.AlignVCenter)
         self.label_2.setObjectName("label_2")
         self.horizontalLayout.addWidget(self.label_2)
         self.gridLayout.addLayout(self.horizontalLayout, 0, 0, 1, 1)
-        self.printTextEdit = QtWidgets.QTextEdit(self.gridLayoutWidget)
+        self.printTextEdit = QtWidgets.QTextEdit(runningDialog)
         self.printTextEdit.setObjectName("printTextEdit")
         self.gridLayout.addWidget(self.printTextEdit, 1, 0, 1, 1)
-        runningDialog.setLayout(self.gridLayout)
-        
-        
-    
-        
+        self.gridLayout_2.addLayout(self.gridLayout, 0, 0, 2, 2)
+        self.loadingGIFLabel = QtWidgets.QLabel(runningDialog)
+        self.loadingGIFLabel.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.loadingGIFLabel.setText("")
+        self.loadingGIFLabel.setAlignment(QtCore.Qt.AlignCenter)
+        self.loadingGIFLabel.setObjectName("loadingGIFLabel")
+        self.gridLayout_2.addWidget(self.loadingGIFLabel, 1, 1, 1, 1)
         """
         self.movie = QtGui.QMovie(resource_path("img/loader-50.gif"), QtCore.QByteArray())
         self.movie.setCacheMode(QtGui.QMovie.CacheAll)
@@ -62,15 +69,16 @@ class Ui_runningDialog(object):
 
     def retranslateUi(self, runningDialog):
         _translate = QtCore.QCoreApplication.translate
-        runningDialog.setWindowTitle(_translate("runningDialog", "MGWR Running"))
+        runningDialog.setWindowTitle(
+            _translate("runningDialog", "MGWR Running"))
         self.label.setText(_translate("runningDialog", "Time Elapsed:"))
         self.label_2.setText(_translate("runningDialog", "00:00:00"))
-    
+
     def restartTimer(self):
         self.printTextEdit.clear()
         self.label_2.setText("00:00:00")
 
-    def stopThread(self,thread):
+    def stopThread(self, thread):
         thread.terminate()
 
     def __init__(self, parent=None, **kwargs):
@@ -86,6 +94,7 @@ class Ui_runningDialog(object):
         self.printTextEdit.setTextCursor(cursor)
         self.printTextEdit.ensureCursorVisible()
 
+
 def resource_path(relative_path):
     if hasattr(sys, '_MEIPASS'):
         return os.path.join(sys._MEIPASS, relative_path)
@@ -93,14 +102,11 @@ def resource_path(relative_path):
 
 
 class EmittingStream(QtCore.QObject):
-    
+
     textWritten = QtCore.pyqtSignal(str)
-    
+
     def write(self, text):
         self.textWritten.emit(str(text))
-    
+
     def flush(self):
         pass
-
-
-
